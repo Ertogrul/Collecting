b9826170ec8b6f43fcef789f0c5b8f34f8b9a7f9
Ertogrul <ertogrulhatemoglu@gmail.com>
2022-07-22 16:44:22 +0300

    first commit

---
+++ Basic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi, adet = 0;
+            int toplam = 0;
+            Console.Write("Kaç tane sayı toplayacaksın= ");
+            adet = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= adet; i++)
+            {
+                Console.Write(i + ". sayıyı giriniz= ");
+                sayi = int.Parse(Console.ReadLine());
+
+                toplam = toplam + sayi;
+            }
+            Console.WriteLine("Girdiğiniz sayıların toplamı= " + toplam);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ adcas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adcas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi, adet = 0;
+            int toplam = 0;
+            Console.WriteLine("Kaç tane sayı toplayacaksın= ");
+            adet = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= adet; i++)
+            {
+                Console.Write(i + ". sayıyı giriniz= ");
+                sayi = int.Parse(Console.ReadLine());
+
+                toplam = toplam + i;
+            }
+            Console.WriteLine("Girdiğiniz sayıların toplamı= " + toplam);
+            Console.ReadKey();
+        }
+    }
+}
